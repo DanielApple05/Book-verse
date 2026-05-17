e1e89ca908c402fb1d39252d4f6c9138900526f3
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-17 14:41:59 +0100

    Enhance Header component with landing page image and styling improvements

--- book-verse-frontend/src/assets/components/header-component/header.jsx
+++ book-verse-frontend/src/assets/components/header-component/header.jsx
@@ -7,20 +7,28 @@ const Header = () => {
 
   return (
     <div>
-      <div className='flex justify-between p-3 '>
-        <h1>
-          Book Verse
-        </h1>
-        <div className='flex space-x-15 w-7/12 '>
-          {
-            Tabs.map((tab) => (
-              <div key={tab.id} className='font-bold'>
-                <p className='cursor-pointer'>
-                  {tab.name}
-                </p>
-              </div>
-            ))
-          }
+      <div>
+        <div className='flex justify-between p-3 border-b-2 border-gray-200 '>
+          <h1>
+            Book Verse
+          </h1>
+          <div className='flex space-x-15 w-7/12 '>
+            {
+              Tabs.map((tab) => (
+                <div key={tab.id} className='font-bold'>
+                  <p className='cursor-pointer'>
+                    {tab.name}
+                  </p>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <div>
+          <img src="/images/landing-page.png" alt="landing-page" className='bg-cover bg-no-repeat flex bg-center h-screen w-full' />
+          <div>
+
+          </div>
         </div>
       </div>
     </div>
