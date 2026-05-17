405fc651d148bd759ceebf098ec16119c2f2e884
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-17 21:42:11 +0100

    Update dependencies and enhance Index page layout with buttons

--- book-verse-frontend/src/assets/pages/index.jsx
+++ book-verse-frontend/src/assets/pages/index.jsx
@@ -2,14 +2,33 @@ import React from 'react';
 import Header from '../components/header-component/header'
 
 const Index = () => {
+
+  const youAre = [{ id: 1, name: "Start Reading" }, { id: 2, name: "Publish" }]
   return (
     <>
       <Header />
-      <div className='bg-cover bg-no-repeat bg-center flex h-screen min-w-full relative' style={{ backgroundImage: 'url(/images/landing-page.png)' }}>
-        <div className='absolute inset-0 flex flex-col justify-center'>
-          <h1 className='text-[40px] font-bold'>Read More</h1>
-          <h1 className='text-[40px] font-bold'>Discover More</h1>
+      <div className='bg-cover bg-no-repeat bg-center flex h-screen min-w-full relative rounded-b-lg border-b border-b-amber-200 shadow-2xl' style={{ backgroundImage: 'url(/images/landing-page.png)' }}>
+        <div className='absolute inset-0 flex flex-col justify-center ml-20'>
+          <h1 className='text-[40px] font-bold'>Read More.</h1>
+          <h1 className='text-[40px] font-bold'>Discover More.</h1>
+          <div className='flex space-x-6 mt-5'>
+            {
+              youAre.map((areYou) => (
+                <button key={areYou.id} className='bg-white p-3 rounded-lg cursor-pointer hover:bg-black hover:text-white font-semibold '>
+                  {areYou.name}
+                </button>
+              ))
+            }
+          </div>
         </div>
+
+      </div>
+      <div className='bg-[#FAF7F2] pt-5'>
+
+        <h1 className='text-center text-2xl font-semibold '>Everything you need in a reading App</h1>
+
+
+
       </div>
 
     </>
--- book-verse-frontend/src/main.jsx
+++ book-verse-frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { browserRouter } from routerDOM 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ 
+ <StrictMode>
     <App />
   </StrictMode>,
 )
