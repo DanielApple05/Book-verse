6818966e159440951340390f8a6f253e7bf8f633
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-19 17:17:30 +0100

    feat: add path properties to navTabs and update sideNavBar for routing

--- book-verse-frontend/src/assets/components/navTabs.jsx
+++ book-verse-frontend/src/assets/components/navTabs.jsx
@@ -1,12 +1,12 @@
 import { faHome, faBook, faCompass, faLayerGroup, faFeatherPointed, faGear } from "@fortawesome/free-solid-svg-icons";
 
 const Tabs = [
-  { id: 1, name: "Home",       icon: faHome },
-  { id: 2, name: "Library",    icon: faBook },
-  { id: 3, name: "Discovery",  icon: faCompass },
-  { id: 4, name: "Categories", icon: faLayerGroup },
-  { id: 5, name: "Authors",    icon: faFeatherPointed },
-  { id: 6, name: "Settings",   icon: faGear }
+  { id: 1, name: "Home",   path: '/home',    icon: faHome },
+  { id: 2, name: "Library",    path: '/library', icon: faBook },
+  { id: 3, name: "Discovery",  path: '/discovery', icon: faCompass },
+  { id: 4, name: "Categories",  path: '/category',  icon: faLayerGroup },
+  { id: 5, name: "Authors",  path: '/authors',  icon: faFeatherPointed },
+  { id: 6, name: "Settings",  path: '/settings',  icon: faGear }
 ];
 
 export default Tabs;
--- book-verse-frontend/src/assets/components/sideNavBar.jsx
+++ book-verse-frontend/src/assets/components/sideNavBar.jsx
@@ -5,14 +5,21 @@ import { NavLink } from "react-router-dom";
 const sideNavBar = () => {
   return (
     <>
-      <div className='w-2/12 h-screen bg-amber-50 flex justify-center'>
-      <div className="space-y-4 p-4">
-        {Tabs.map(tab => ( 
-          <NavLink key={tab.id} className="flex items-center gap-6 bg-red-100 rounded-lg p-2 font-semibold text-xl">
-            <FontAwesomeIcon icon={tab.icon} className="text-gray-500 text-xl"/>
-            <span className="">{tab.name}</span>
-          </NavLink>
-        ))}
+      <div className='w-2/12 h-screen bg-[#F5E6D0] flex justify-center'>
+        <div className="space-y-4 p-4">
+          {Tabs.map(tab => (
+            <NavLink
+              key={tab.id}
+              to={tab.path}
+              className={({ isActive }) =>
+                `flex items-center gap-6 rounded-lg p-2 font-semibold text-xl ${isActive ? 'bg-blue-500 text-white' : 'bg-gray-100 text-gray-700'
+                }`
+              }
+            >
+              <FontAwesomeIcon icon={tab.icon} className="text-xl" />
+              <span>{tab.name}</span>
+            </NavLink>
+          ))}
         </div>
       </div>
     </>
