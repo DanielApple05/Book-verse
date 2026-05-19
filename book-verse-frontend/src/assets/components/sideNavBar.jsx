fed96ca55808fc1d958be8a21ebd514369fe295a
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-19 18:43:15 +0100

    feat: add axios for API calls and implement book fetching in SideNavBar component

--- book-verse-frontend/src/assets/components/sideNavBar.jsx
+++ book-verse-frontend/src/assets/components/sideNavBar.jsx
@@ -1,29 +1,46 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Tabs from './navTabs'
+import { useState, useEffect } from "react";
+import Tabs from './navTabs';
 import { NavLink } from "react-router-dom";
 
-const sideNavBar = () => {
+const SideNavBar = () => {
+  const [books, setBooks] = useState([]);
+  const API_KEY = import.meta.env.VITE_GOOGLE_BOOKS_KEY;
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=fiction&key=${API_KEY}`
+      );
+      const data = await response.json();
+      setBooks(data.items);
+      console.log(fetchBooks)
+    };
+
+    fetchBooks();
+  }, []);
+
+
   return (
-    <>
-      <div className='w-2/12 h-screen bg-[#F5E6D0] flex justify-center'>
-        <div className="space-y-4 p-4">
-          {Tabs.map(tab => (
-            <NavLink
-              key={tab.id}
-              to={tab.path}
-              className={({ isActive }) =>
-                `flex items-center gap-6 rounded-lg p-2 font-semibold text-xl ${isActive ? 'bg-blue-500 text-white' : 'bg-gray-100 text-gray-700'
-                }`
-              }
-            >
-              <FontAwesomeIcon icon={tab.icon} className="text-xl" />
-              <span>{tab.name}</span>
-            </NavLink>
-          ))}
-        </div>
+    <div className='w-2/12 h-screen bg-[#F9F6F1] flex justify-center'>
+      <div className="space-y-4 p-4">
+        {Tabs.map(tab => (
+          <NavLink
+            key={tab.id}
+            to={tab.path}
+            className={({ isActive }) =>
+              `flex items-center gap-6 rounded-lg p-2 font-semibold text-xl ${
+                isActive ? 'bg-blue-500 text-white' : 'bg-gray-100 text-gray-700'
+              }`
+            }
+          >
+            <FontAwesomeIcon icon={tab.icon} className="text-xl" />
+            <span>{tab.name}</span>
+          </NavLink>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
-export default sideNavBar;
+export default SideNavBar;
