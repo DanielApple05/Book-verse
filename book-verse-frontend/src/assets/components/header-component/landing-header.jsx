df4fa4b4f78ebae11409129f850d41b1c86ac692
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-23 10:36:48 +0100

    feat: enhance Header component with FontAwesome icons and improve layout in index page

--- book-verse-frontend/src/assets/components/header-component/landing-header.jsx
+++ book-verse-frontend/src/assets/components/header-component/landing-header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpen } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
 
@@ -7,22 +9,23 @@ const Header = () => {
 
   return (
     <>
-        <div className='flex justify-between p-5 border-b border-gray-200 fixed z-50 w-full bg-white'>
-          <h1>
-            Book Verse
-          </h1>
-          <div className='flex space-x-15 w-7/12 '>
-            {
-              Tabs.map((tab) => (
-                <div key={tab.id} className='font-bold'>
-                  <p className='cursor-pointer'>
-                    {tab.name}
-                  </p>
-                </div>
-              ))
-            }
-          </div>
+      <div className='flex justify-between p-5 border-b border-gray-200 fixed z-50 w-full bg-white'>
+        <div className='flex items-center font-bold space-x-2'>
+          <FontAwesomeIcon icon={faBookOpen} className='text-[#1A1A2E]' />
+          <p> BookVerse </p>
         </div>
+        <div className=' xl:flex hidden space-x-15 w-7/12 '>
+          {
+            Tabs.map((tab) => (
+              <div key={tab.id} className='font-bold'>
+                <p className='cursor-pointer'>
+                  {tab.name}
+                </p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </>
   );
 }
--- book-verse-frontend/src/assets/pages/index.jsx
+++ book-verse-frontend/src/assets/pages/index.jsx
@@ -8,14 +8,15 @@ const Index = () => {
   return (
     <>
       <Header />
-      <div className='bg-cover bg-no-repeat bg-center flex h-screen min-w-full relative rounded-b-lg border-b border-b-amber-200 shadow-2xl' style={{ backgroundImage: 'url(/images/landing-page.png)' }}>
-        <div className='absolute inset-0 flex flex-col justify-center ml-20'>
-          <h1 className='text-[40px] font-bold'>Read More.</h1>
-          <h1 className='text-[40px] font-bold'>Discover More.</h1>
-          <div className='flex space-x-6 mt-5'>
+      <div className='bg-cover bg-no-repeat bg-center flex xl:h-screen h-80 min-w-full relative rounded-b-lg border-b border-b-amber-200 shadow-2xl inset-0 ' style={{ backgroundImage: 'url(/images/landing-page.png)' }}>
+      <div className='absolute inset-0 bg-black/30 rounded-xl' />
+        <div className='absolute inset-0 flex flex-col justify-center xl:ml-20 ml-5 text-white'>
+          <h1 className='xl:text-4xl text-xl  font-bold'>Read More.</h1>
+          <h1 className='xl:text-4xl text-xl font-bold'>Discover More.</h1>
+          <div className='flex xl:text-base text-sm xl:space-x-6 space-x-2 mt-5'>
             {
               youAre.map((areYou) => (
-                <Link to='/signIn' key={areYou.id} className='bg-white p-3 rounded-lg cursor-pointer hover:bg-[#1B1F3B] hover:text-white font-semibold'>
+                <Link to='/signIn' key={areYou.id} className='bg-white p-3 text-black rounded-lg cursor-pointer hover:bg-[#1B1F3B] hover:text-white font-semibold'>
                   {areYou.name}
                 </Link>
               ))
