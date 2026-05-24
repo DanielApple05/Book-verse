ffec7ae79283253972d279a03da6b9a5d23745aa
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-24 04:07:18 +0100

    fixed icon size to fit different responsive screen sizes

--- book-verse-frontend/src/assets/components/header-component/header.jsx
+++ book-verse-frontend/src/assets/components/header-component/header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <>
       <div className='flex items-center justify-between p-4 border-b xl:text-2xl text-sm  border-gray-200 w-full fixed top-0 z-50 bg-white '>
         <div className='flex items-center font-bold space-x-2'>
-          <FontAwesomeIcon icon={faBookOpen} className='text-[#1A1A2E]' />
+          <FontAwesomeIcon icon={faBookOpen} className='text-[#1A1A2E] text-xl ' />
           <p> BookVerse </p>
         </div>
         <div className='xl:flex hidden w-5/6 justify-center'> <Search /></div>
