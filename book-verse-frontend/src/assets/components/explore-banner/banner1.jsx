558739fef9be8158aee5223c44dc67cfc269c064
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-25 08:49:22 +0100

    fix: update layout and styling in Banner1 component for better alignment and responsiveness

--- book-verse-frontend/src/assets/components/explore-banner/banner1.jsx
+++ book-verse-frontend/src/assets/components/explore-banner/banner1.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Banner1 = () => {
   return (
     <div className='bg-cover bg-center h-72 relative my-6 bg-no-repeat w-full rounded-xl' style={{ backgroundImage: 'url(/images/banner-img1.png)' }}>
       <div className='absolute inset-0 bg-black/30 rounded-xl' />
-      <div className='absolute inset-0 flex items-center justify-between xl:px-10 px-2 xl:text-base text-sm'>
-        <div>
-          <p className='font-bold text-xl text-white'>Keep Reading, Keep Growing</p>
+      <div className='absolute inset-0 flex  flex-col items-center justify-center xl:px-10 px-2 xl:text-base text-sm'>
+        <div className='xl:text-xl text-sm'>
+          <p className='font-bold text-center text-white'>Keep Reading, Keep Growing</p>
           <p className='text-white/80 mt-1'>You're doing great, many more adventures ahead</p>
         </div>
-        <button className='bg-black text-white xl:px-6 px-2 py-3 cursor-pointer rounded-lg hover:bg-gray-800 transition-all whitespace-nowrap '>
-          Explore more books
-        </button>
+        <Link to='/category' 
+        className='bg-black text-white xl:px-6 px-2 py-3 xl:text-base text-xs cursor-pointer rounded-lg hover:bg-gray-800 transition-all whitespace-nowrap mt-5 '>
+            Explore more books
+          category
+        </Link>
       </div>
     </div>
   );
