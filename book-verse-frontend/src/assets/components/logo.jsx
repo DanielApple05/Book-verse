33b5616ff659d1fec7eee06439f79dcbc7ba77e7
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-25 16:09:57 +0100

    feat: add Logo component for improved branding and navigation

---
+++ book-verse-frontend/src/assets/components/logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+const Logo = () => {
+  return (
+    <>
+       <Link to='/home' className='flex items-center font-bold space-x-2'>
+          <FontAwesomeIcon icon={faBookOpen} className='text-[#1A1A2E] text-xl ' />
+          <p> BookVerse </p>
+        </Link>
+    </>
+  );
+}
+
+export default Logo;
