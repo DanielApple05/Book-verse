883100b4a796ad323ca7f5c252d88695cf586d5c
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-28 20:39:03 +0100

    feat: enhance header component to display user's first letter from username if authenticated

--- book-verse-frontend/src/assets/components/header-component/header.jsx
+++ book-verse-frontend/src/assets/components/header-component/header.jsx
@@ -4,14 +4,23 @@ import { faBookOpen, faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import Search from '../search';
 import { Link } from 'react-router-dom';
 import Logo from '../logo';
+import { jwtDecode } from 'jwt-decode';
 
 const Header = () => {
+  const token = localStorage.getItem('token');
+  const isValidToken = token && token !== 'undefined' && token.split('.').length === 3;
+  const user = isValidToken ? jwtDecode(token) : null;
+  const firstLetter = user?.username?.charAt(0).toUpperCase();
+
   return (
     <>
       <div className='flex items-center justify-between p-4 border-b xl:text-2xl text-sm  border-gray-200 w-full fixed top-0 z-50 bg-white '>
         <Logo />
         <div className='xl:flex hidden w-5/6 justify-center'> <Search /></div>
-        <FontAwesomeIcon icon={faCircleUser} className='text-[#1A1A2E] text-2xl ' />
+        <div className='flex items-center space-x-2'>
+      { user ? (<span className='font-semibold'>{firstLetter}</span>) : (<Link to='/login'><FontAwesomeIcon icon={faCircleUser} className='text-[#1A1A2E] text-2xl ' /></Link>) }
+         
+        </div>
       </div>
     </>
   );
