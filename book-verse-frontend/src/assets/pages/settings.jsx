ba33a6f84d48ced6a9f58fb043274fb11a727c28
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-31 23:15:47 +0100

    feat: enhance Settings page layout with improved spacing and styling for better user experience

--- book-verse-frontend/src/assets/pages/settings.jsx
+++ book-verse-frontend/src/assets/pages/settings.jsx
@@ -11,10 +11,10 @@ const Settings = () => {
       <Header />
       <div className='flex'>
         <SideNavBar />
-        <div className='mt-20 flex-1 pl-5 bg-amber-50'>
+        <div className='mt-20 flex-1 p-5 bg-amber-50'>
           <h1 className='text-2xl font-bold '>Settings</h1>
           <p> Manage your preference and account settings </p>
-          <div className='bg-white rounded p-3 w-70 space-y-3 shadow-2xl'>
+          <div className='bg-white rounded p-3 xl:w-70 w-full space-y-3 shadow-2xl'>
             <div className='flex items-center gap-4 border-b border-b-gray-200 pb-2'>
               <FontAwesomeIcon icon={faCircleUser} />
               <p>Account</p>
@@ -35,9 +35,9 @@ const Settings = () => {
               <FontAwesomeIcon icon={faArrowRight} />
             </div>
           </div>
-          <div className='bg-white rounded p-3 w-70 space-y-3 shadow-2xl mt-5'>
-            <p>Preferences</p>
-            <div className='flex'>
+          <div className='bg-white rounded p-3 xl:w-70 w-full space-y-3 shadow-2xl mt-5'>
+            <p className='font-bold  border-b border-b-gray-200 pb-2'>Preferences</p>
+            <div className='flex items-center justify-between gap-4 border-b border-b-gray-200'>
               <div>
                 <p> Theme</p>
                 <p className='text-xs'> Choose your preferred theme</p>
@@ -47,13 +47,12 @@ const Settings = () => {
                 <button className='p-1 rounded-xl border hover:text-white hover:bg-blue-950'>Dark</button>
               </div>
             </div>
-            <div className='flex items-center '>
+            <div className='flex items-center justify-between gap-4 '>
               <div>
                 <p>Auto Dark Mode </p>
                 <p className='text-xs'> Switch to dark mode at night </p>
               </div>
-            <input type="checkbox" name="" id="" />
-          
+            <input type="checkbox" />
             </div>
           </div>
         </div>
