6072ab480391de5a519047a45d17d484a3d60655
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-31 21:35:44 +0100

    feat: enhance DiscoveryPageCard layout with additional components for better book discovery

--- book-verse-frontend/src/assets/components/discovery-component/discoveryPageCard.jsx
+++ book-verse-frontend/src/assets/components/discovery-component/discoveryPageCard.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Header from '../header-component/header';
 import Banner3 from '../explore-banner/banner3';
 import Search from '../search';
+import TrendingBooks  from '../book-components/trendingBooks';
+import NewArrivals from '../book-components/newArrivals';
+import Categories from '../category-component/categories';
 
 const DiscoveryPageCard = () => {
   return (
     <div>
-         <div className='xl:hidden flex mb-3 bg-white/30 sticky top-14 rounded-lg '> <Search /> </div>
+     <div className='xl:hidden flex mb-3 bg-white/30 sticky top-14 rounded-lg '> <Search /> </div>
+     <h1 className='text-2xl font-bold mb-2'>Discover New Books</h1>
+     <p className='text-gray-500 mb-10'>Explore our curated selection of books across various genres and categories. Find your next great read today!</p>
       <Banner3/>
+      <Categories />
+      <TrendingBooks />
+      <NewArrivals />
     </div>
   );
 }
