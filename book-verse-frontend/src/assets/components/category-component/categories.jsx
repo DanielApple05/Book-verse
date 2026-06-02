f06ea694b57c4593dd5b67fdc6fb3a01d78ff49c
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-02 22:54:41 +0100

    feat: enhance category component with dark mode styles for improved UI

--- book-verse-frontend/src/assets/components/category-component/categories.jsx
+++ book-verse-frontend/src/assets/components/category-component/categories.jsx
@@ -15,19 +15,19 @@ const Categories = () => {
     }
   };
   return (
-    <div className='my-6 '>
+    <div className='my-6 dark:text-gray-300 text-black'>
       <h1 className='flex items-center text-lg font-semibold '>Popular Categories</h1>
-      <div className='flex py-2 gap-3 w-full overflow-x-auto'>
+      <div className='flex py-2 gap-3 w-full overflow-x-auto '>
         {categoryTabs.map((tab, index) => (
           <div
             onClick={() => handleClick(tab.tab)}
             key={tab.id}
-            className='flex flex-col items-center gap-2 p-4 rounded-xl border border-gray-100 bg-white shadow-sm hover:shadow-md hover:-translate-y-1 transition-all cursor-pointer min-w-24'
+            className='flex flex-col items-center gap-2 p-4 rounded-xl border border-gray-100 dark:bg-gray-800 bg-white shadow-sm hover:shadow-md hover:-translate-y-1 transition-all cursor-pointer min-w-24'
           >
             <div className={`w-10 h-10 rounded-full flex items-center justify-center ${iconColors[index]}`}>
               <FontAwesomeIcon icon={tab.icon}  className='text-sm' />
             </div>
-            <span className='text-xs font-semibold text-gray-700 capitalize'>{tab.tab}</span>
+            <span className='text-xs font-semibold capitalize'>{tab.tab}</span>
           </div>
         ))}
       </div>
