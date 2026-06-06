c7461a72d82d1dd344e166f2e1e4847bb53d8420
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-06 04:22:21 +0100

    feat: update Header component for improved dark mode support and styling consistency

--- book-verse-frontend/src/assets/components/header-component/landing-header.jsx
+++ book-verse-frontend/src/assets/components/header-component/landing-header.jsx
@@ -7,17 +7,17 @@ const Header = () => {
 
 
   return (
-    <>
-      <div className='flex justify-between p-5 border-b border-gray-200 fixed z-50 w-full bg-white'>
-        <div className='flex items-center font-bold space-x-2'>
-          <FontAwesomeIcon icon={faBookOpen} className='text-[#1A1A2E]' />
-          <p> BookVerse </p>
-        </div>
-        <Link to='/signin' className='cursor-pointer p-2 rounded-xl shadow-2xl bg-amber-100 hover:text-white hover:bg-[#1B1F3B]'>
-           Sign in 
-        </Link>
+
+    <div className='flex justify-between p-5 border-b border-gray-200 fixed z-50 top-0 w-full dark:bg-gray-800 '>
+      <div className='flex items-center font-bold space-x-2'>
+        <FontAwesomeIcon icon={faBookOpen} className='text-[#1A1A2E] dark:text-white' />
+        <p> BookVerse </p>
       </div>
-    </>
+      <Link to='/signin' className='cursor-pointer p-2 rounded-xl shadow-2xl bg-amber-100 dark:text-black font-semibold hover:text-white hover:bg-[#1B1F3B]'>
+        Sign in
+      </Link>
+    </div>
+
   );
 }
 
