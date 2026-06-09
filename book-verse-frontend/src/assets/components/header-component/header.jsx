466afa8aa5ad6078d793dd0f098aace7e3b69049
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-09 10:36:35 +0100

    refactor: simplify user authentication logic in Header component

--- book-verse-frontend/src/assets/components/header-component/header.jsx
+++ book-verse-frontend/src/assets/components/header-component/header.jsx
@@ -5,32 +5,27 @@ import { Link } from 'react-router-dom';
 import Logo from '../logo';
 import { jwtDecode } from 'jwt-decode';
 import { useTheme } from '../../../context/themeContext';
+import { getUserFromToken } from '../../../utils';
+import { getToken } from '../../../helpers';
 
 const Header = () => {
-  const token = localStorage.getItem('token');
-  const isValidToken = token && token !== 'undefined' && token.split('.').length === 3;
-  let user = null;
-  try {
-    if (isValidToken) {
-      user = jwtDecode(token);
-    }
-  } catch (err) {
-    console.error('Invalid token');
-  }
+  const user = getUserFromToken()
+  const Token = getToken()
+  const isLoggedIn = !!Token
   const firstLetter = user?.username?.charAt(0).toUpperCase();
   const { darkMode, toggleDarkMode } = useTheme();
 
   return (
-  <div className='flex items-center justify-between p-4 border-b xl:text-2xl text-sm border-gray-200 w-full fixed top-0 z-50 dark:bg-gray-800/50 backdrop-blur-md ' >
+    <div className='flex items-center justify-between p-4 border-b xl:text-2xl text-sm border-gray-200 w-full fixed top-0 z-50 dark:bg-gray-800/50 backdrop-blur-md ' >
       <Logo />
-        <Search />
+      <Search />
       <div className='flex items-center gap-7'>
         <FontAwesomeIcon
           icon={darkMode ? faSun : faMoon}
           onClick={toggleDarkMode}
           className='cursor-pointer text-[#1A1A2E] dark:text-white text-2xl '
         />
-        {user ? (
+        {isLoggedIn ? (
           <span className='w-8 h-8 bg-[#E8834A] text-xl rounded-full text-center font-semibold flex items-center justify-center'>
             {firstLetter}
           </span>
