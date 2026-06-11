09fd1890477f8bb5bbe0039bcc566a11b5fa1e39
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-11 19:36:44 +0100

    feat: update Contact component layout and styling for improved spacing and responsiveness

--- book-verse-frontend/src/assets/pages/contact.jsx
+++ book-verse-frontend/src/assets/pages/contact.jsx
@@ -9,11 +9,11 @@ const Contact = () => {
   return (
     <>
       <Header />
-      <div style={{ backgroundImage: 'url(/images/contact-hero.png)' }} className='bg-cover bg-center h-72 lg:h-[80vh] relative bg-no-repeat mt-15  '>
-        <div className='absolute inset-0 bg-black/50 ' />
+      <div style={{ backgroundImage: 'url(/images/contact-hero.png)' }} className='bg-cover bg-center h-72 lg:h-[80vh] relative bg-no-repeat mt-16'>
+        <div className='absolute inset-0 bg-black/50' />
       </div>
-      <div className='flex xl:flex-row flex-col'>
-        <div className='xl:m-10 m-5 xl:p-10 p-5 xl:w-[50%] w-full shadow-2xl rounded'>
+      <div className='flex flex-col xl:flex-row'>
+        <div className='xl:m-10 m-0  xl:p-10 p-5 xl:w-[50%] w-full shadow-2xl rounded'>
           <div className='space-y-2'>
             <div className='flex items-center gap-3 font-semibold'>
               <FontAwesomeIcon icon={faEnvelope} />
@@ -21,15 +21,15 @@ const Contact = () => {
             </div>
             <p className='text-sm'> Fill out the form below and we'll get back to you as soon as possible</p>
           </div>
-          <form className='grid space-y-5 mt-2 pt-3 '>
-            <div className='flex justify-between gap-5 '>
-              <div className='flex flex-col gap-2 '>
+          <form className='grid space-y-5 mt-2 pt-3'>
+            <div className='flex flex-col md:flex-row gap-5'>
+              <div className='flex flex-col gap-2 w-full'>
                 <label className='font-semibold text-sm'> Your Name</label>
-                <input type="text" className='outline-none border border-gray-200 rounded' />
+                <input type="text" className='outline-none border border-gray-200 rounded w-full p-2' />
               </div>
-              <div className='flex flex-col gap-2'>
+              <div className='flex flex-col gap-2 w-full'>
                 <label className='font-semibold text-sm'> Your Email</label>
-                <input type="text" className='outline-none border border-gray-200 rounded' />
+                <input type="text" className='outline-none border border-gray-200 rounded w-full p-2' />
               </div>
             </div>
             <div className='flex flex-col gap-2'>
@@ -42,10 +42,10 @@ const Contact = () => {
               <label className='font-semibold text-sm'> Message</label>
               <textarea className='outline-none border border-gray-200 rounded h-32' />
             </div>
-            <button className='bg-amber-700 p-2 w-60 cursor-pointer rounded-lg font-semibold'> Send Message </button>
+            <button className='bg-amber-700 p-2 w-full md:w-60 cursor-pointer rounded-lg font-semibold'> Send Message </button>
           </form>
         </div>
-        <div className='w-[50%]'>
+        <div className='xl:w-[50%] w-full xl:p-10 p-5'>
           hello
         </div>
       </div>
