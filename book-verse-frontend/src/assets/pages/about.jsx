3bc5d39f008f5ac69ec09e4bff96be9bf6665f91
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-11 21:09:49 +0100

    feat: implement About page layout with background image and content

--- book-verse-frontend/src/assets/pages/about.jsx
+++ book-verse-frontend/src/assets/pages/about.jsx
@@ -8,9 +8,13 @@ const About = () => {
   return (
     <>
       <Header />
-       <div>
-        
-       </div>
+      <div style={{ backgroundImage: 'url(/images/about-us-hero.png)' }} className='bg-cover bg-center h-72 lg:h-[80vh] relative bg-no-repeat mt-16'>
+        <div className='absolute inset-0 bg-black/50' />
+        <div className='absolute inset-0 flex flex-col px-5 w-[40%]'>
+          <h1 className='text-white text-3xl xl:text-5xl font-bold'>Our story</h1>
+          <p className='text-white/80 mt-3 max-w-2xl'>We'd love to hear from you. Whether you have a question, feedback, or just want to say hello, send us a message.</p>
+        </div>
+      </div>
       <Footer />
       <MobileNavBar />
     </>
