dc7d7702aa6eb1b0828dacc6733dd244bf08e18f
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-11 23:02:02 +0100

    feat: update About page content and layout for improved storytelling and engagement

--- book-verse-frontend/src/assets/pages/about.jsx
+++ book-verse-frontend/src/assets/pages/about.jsx
@@ -10,9 +10,18 @@ const About = () => {
       <Header />
       <div style={{ backgroundImage: 'url(/images/about-us-hero.png)' }} className='bg-cover bg-center h-72 lg:h-[80vh] relative bg-no-repeat mt-16'>
         <div className='absolute inset-0 bg-black/50' />
-        <div className='absolute inset-0 flex flex-col px-5 w-[40%]'>
+        <div className='absolute inset-0 flex flex-col p-10 w-[40%] space-y-5'>
+          <span className='text-sm text-red-800 font-bold'>ABOUT US</span>
           <h1 className='text-white text-3xl xl:text-5xl font-bold'>Our story</h1>
-          <p className='text-white/80 mt-3 max-w-2xl'>We'd love to hear from you. Whether you have a question, feedback, or just want to say hello, send us a message.</p>
+          <p className='text-white/80 mt-3 max-w-2xl'>Bookverse was born out of a simple belief - books have the power to inspire, educate, and transform lives.</p>
+          <p> We created BookVerse to be more than just a place to read. It's a community for book lovers to discover and connect, and grow fast.</p>
+          <button className='bg-amber-800 p-2 rounded w-60 cursor-pointer hover:bg-amber-950 '>Explore Our Lirary</button>
+        </div>
+      </div>
+      <div>
+        <div className='flex flex-col space-y-3 justify-center items-center '>
+          <p className='text-red-800 font-bold text-sm'>WHAT WE STAND FOR </p>
+          <p className='text-xl font-semibold '>Our Values</p>
         </div>
       </div>
       <Footer />
