75f16737afb2f8061a1066aca07734288426da4b
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-12 00:28:57 +0100

    feat: add Values component and integrate into About page for enhanced storytelling

---
+++ book-verse-frontend/src/assets/components/value.jsx
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpen, faPeopleGroup, faLightbulb, faShield } from '@fortawesome/free-solid-svg-icons';
+
+const Value = [ 
+  {
+    id: 1,
+    logo: faBookOpen,
+    topic: "Passion For Reading",
+    text: " We are passionate about books and the impact they have on individuals and communities"
+  },
+    {
+    id: 2,
+    logo: faPeopleGroup,
+    topic: "Community",
+    text: " We believe in the power of connection and building a community of readers"
+  },
+    {
+    id: 3,
+    logo: faLightbulb,
+    topic: "Growth",
+    text: "We encourage learning, curiousity and personal growth through reading"
+  },
+    {
+    id: 4,
+    logo: faShield,
+    topic: "Trust And Quality",
+    text: "We are committed to providing quality content and a safe reliable experience "
+  }
+ ]
+
+export default Value;
--- book-verse-frontend/src/assets/pages/about.jsx
+++ book-verse-frontend/src/assets/pages/about.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../components/header-component/header';
 import Footer from '../components/footerComponents/footer';
 import MobileNavBar from '../components/navigations/mobileNavBar';
+import Value from '../components/value';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const About = () => {
@@ -13,15 +15,26 @@ const About = () => {
         <div className='absolute inset-0 flex flex-col xl:p-10 p-5 xl:w-[40%] w-full xl:space-y-5 space-y-3'>
           <span className='text-sm text-red-800 font-bold'>ABOUT US</span>
           <h1 className='text-white text-2xl xl:text-5xl font-bold'>Our story</h1>
-          <p className='text-white text-xs'>Bookverse was born out of a simple belief - books have the power to inspire, educate, and transform lives.</p>
-          <p className='text-white text-xs '> We created BookVerse to be more than just a place to read. It's a community for book lovers to discover and connect, and grow fast.</p>
+          <p className='text-white xl:text-base text-xs'>Bookverse was born out of a simple belief - books have the power to inspire, educate, and transform lives.</p>
+          <p className='text-white xl:text-base text-xs '> We created BookVerse to be more than just a place to read. It's a community for book lovers to discover and connect, and grow fast.</p>
           <button className='bg-amber-800 p-2 rounded w-60 cursor-pointer hover:bg-amber-950 font-semibold '>Explore Our Lirary</button>
         </div>
       </div>
       <div>
-        <div className='flex flex-col space-y-3 justify-center items-center '>
+        <div className='flex flex-col space-y-3 justify-center items-center mt-5 '>
           <p className='text-red-800 font-bold text-sm'>WHAT WE STAND FOR </p>
           <p className='text-xl font-semibold '>Our Values</p>
+        </div>
+        <div className=' xl:flex grid grid-cols-2 justify-between items-center xl:px-15 px-5 py-5 xl:space-y-0 space-y-5 '>
+          {
+            Value.map((value) => (
+              <div className='flex flex-col justify-center items-center text-center xl:w-[20%] w-[80%] my-auto '>
+                <div className='rounded-full p-2 bg-orange-200'> <FontAwesomeIcon icon={value.logo} className='text-orange-950' /> </div>
+                <p className='font-semibold'>{value.topic}</p>
+                <p className='xl:text-base text-sm'>{value.text}</p>
+              </div>
+            ))
+          }
         </div>
       </div>
       <Footer />
