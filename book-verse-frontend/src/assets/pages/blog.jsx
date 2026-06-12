5d62a7c398cd624474a949bf95450655d011ce0d
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-12 11:05:36 +0100

    feat: update Search component styling and add MobileNavBar to Blog page

--- book-verse-frontend/src/assets/pages/blog.jsx
+++ book-verse-frontend/src/assets/pages/blog.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/header-component/header';
 import { Link } from 'react-router-dom';
+import MobileNavBar from '../components/navigations/mobileNavBar';
 
 const Blog = () => {
   return (
@@ -11,9 +12,10 @@ const Blog = () => {
         <div className='absolute inset-0 flex flex-col justify-center xl:p-10 p-5 xl:w-[40%] w-full xl:space-y-5 space-y-3'>
           <h1 className='text-white text-2xl xl:text-5xl font-bold'>Our Blog</h1>
           <p className='text-white xl:text-base text-xs'> Explore bookish insights, reading tips, author interviews, and everything for book lovers.</p>
-          <Link className='bg-amber-800 p-2 rounded w-20 text-center  cursor-pointer hover:bg-amber-950 font-bold '>Home</Link>
+          <Link to='/home' className='bg-amber-800 p-2 rounded w-20 text-center  cursor-pointer hover:bg-amber-950 font-bold '>Home</Link>
         </div>
       </div>
+      <MobileNavBar />
     </>
   );
 }
