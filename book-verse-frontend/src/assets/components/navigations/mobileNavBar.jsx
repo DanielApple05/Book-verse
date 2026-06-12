f6ba302b5bba88d57abda72ad096111674b6d4cc
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-12 03:08:09 +0100

    feat: implement search functionality in mobile navigation bar

--- book-verse-frontend/src/assets/components/navigations/mobileNavBar.jsx
+++ book-verse-frontend/src/assets/components/navigations/mobileNavBar.jsx
@@ -1,26 +1,56 @@
-import React from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Tabs from '../navigations/navTabs'
 import { NavLink } from 'react-router-dom';
+import Search from '../search';
 
-const  MobileNavBar = () => {
+const MobileNavBar = () => {
+
+  const [searchOpen, setSearchOpen] = useState(false)
   return (
-     <div className='w-full dark:bg-gray-800 bg-[#F9F6F1] fixed bottom-0 flex xl:hidden dark:text-white p-3'>
-        <div className="space-x-4 flex w-full justify-around">
-          {Tabs.map(tab => (
+    <div className='w-full dark:bg-gray-800 bg-[#F9F6F1] fixed bottom-0 flex xl:hidden dark:text-white p-3'>
+      {searchOpen && (
+        <>
+          <div
+            className="fixed inset-0"
+            onClick={() => setSearchOpen(false)}
+          />
+
+          <div className="fixed bottom-20 left-0 right-0 z-50">
+            <Search />
+          </div>
+        </>
+      )}
+      <div className="space-x-4 flex w-full justify-around">
+        {Tabs.map(tab => (
+          tab.action === "search" ? (
+            <button
+              key={tab.id}
+              onClick={() => setSearchOpen(!searchOpen)}
+            >
+              <FontAwesomeIcon
+                icon={tab.icon}
+                className="text-xl" />
+            </button>
+          ) : (
             <NavLink
               key={tab.id}
               to={tab.path}
               className={({ isActive }) =>
-                `flex items-center gap-3 rounded-lg p-2 font-semibold xl:text-sm text-xs hover:bg-[#cd8f6b]  ${isActive ? 'bg-[#E8834A]' : ''
-                }`
+                `flex items-center gap-3 rounded-lg p-2 font-semibold xl:text-sm text-xs hover:bg-[#cd8f6b]
+             ${tab.style || ""}
+             ${isActive ? "bg-[#E8834A]" : ""}`
+
               }
             >
-              <FontAwesomeIcon icon={tab.icon} className={`text-xl ${tab.style}`} />
+              <FontAwesomeIcon
+                icon={tab.icon}
+                className="text-xl" />
             </NavLink>
-          ))}
-        </div>
+          )
+        ))}
       </div>
+    </div>
   );
 }
 
--- book-verse-frontend/src/assets/components/navigations/navTabs.jsx
+++ book-verse-frontend/src/assets/components/navigations/navTabs.jsx
@@ -1,12 +1,13 @@
-import { faHome, faBook, faCompass, faLayerGroup, faFeatherPointed, faGear } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faBook, faCompass, faLayerGroup, faFeatherPointed, faGear, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Tabs = [
   { id: 1, name: "Home",   path: '/home',    icon: faHome },
   { id: 2, name: "Library",    path: '/library', icon: faBook },
-  { id: 3, name: "Discovery",  path: '/discovery', icon: faCompass },
+  { id: 3, name: "Discovery",  path: '/discovery', icon: faCompass, style: "hidden md:block"  },
   { id: 4, name: "Categories",  path: '/category',  icon: faLayerGroup },
-  { id: 5, name: "Authors",  path: '/authors',  icon: faFeatherPointed, style: "hidden:md" },
-  { id: 6, name: "Settings",  path: '/settings',  icon: faGear }
+  { id: 5, name: "Authors",  path: '/authors',  icon: faFeatherPointed},
+  {  id:6, name: "Search", action: "search", icon: faMagnifyingGlass, style: "hidden md:flex" },
+  { id: 7, name: "Settings",  path: '/settings',  icon: faGear }
 ];
 
 export default Tabs;
